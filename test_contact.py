b26bf11bac0221e95595b6516f45ab397efb97d4
tocomarvin <61111167+tocomarvin@users.noreply.github.com>
2020-05-03 22:22:03 +0800

    Unit test for fallback function of contact

---
+++ test_contact.py
@@ -0,0 +1,13 @@
+from bot import AIchat
+import unittest
+
+class TestCode(unittest.TestCase):
+  def testsimple(self):
+    fallbackOptions = ["Can you be more specific for me?",
+                      "I don't quite understand on  what you ask about the email/contact number of your desired department.",
+                      "Can you be more specific on your inquiry?"]
+    testMsg = "Contact and email of EECE deparment."
+
+    self.assertIn(AIchat.contact(self,testMsg),fallbackOptions)
+if __name__=='__main__':
+  unittest.main()
